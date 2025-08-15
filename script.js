ee67612aecddfc789d6836e62fe53b2ca655d9b0
Daniel Fernández <93385389+Daniel-Fdz@users.noreply.github.com>
2025-08-16 00:48:44 +0200

    Add one round gameplay

--- script.js
+++ script.js
@@ -14,6 +14,32 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Choose between rock, paper or scissors:").toLowerCase();
+    let choice = prompt("Choose between rock, paper and scissors:").toLowerCase();
+    return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "rock" ||
+        humanChoice === "paper" && computerChoice === "paper" ||
+        humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+               humanChoice === "paper" && computerChoice === "rock" ||
+               humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice === "rock" && computerChoice === "paper" ||
+               humanChoice === "paper" && computerChoice === "scissors" ||
+               humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
 
